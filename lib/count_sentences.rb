74b025a4bc4b7ae380469419552ef836fa145612
Gerald Gorman <gpgorman158@gmail.com>
2022-03-18 13:01:44 -0400

    commit tree bcc41b3952aa068d926e9fab0b87b0299f2efc73

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,45 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end 
   end
 
   def count_sentences
-
+    new_array = self.split(/\.|\?|\!/)
+    filtered_array = new_array.select do |element|
+      element.length != 0
+    end
+    filtered_array.length
   end
+  
+  puts "this is a strng with a period.".sentence?
+  puts "this is a strng with a period".sentence?
+  puts "this is a strng with a period?".question?
+  puts "this is a strng with a period".question?
+  puts "this is a strng with a period!".exclamation?
+  puts "this is a strng with a period".exclamation?
+  puts "this... is two?".count_sentences
 end
+
+
+
